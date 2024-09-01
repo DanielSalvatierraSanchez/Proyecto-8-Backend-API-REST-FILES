219242754da2f75753895052168e594d046278cb
dsalvatierra <dss250583@gmail.com>
2024-09-01 09:41:47 +0200

    FIX all files of ingredient completed

--- src/api/controllers/ingredient.js
+++ src/api/controllers/ingredient.js
@@ -5,7 +5,7 @@ const createIngredient = async (req, res, next) => {
     try {
         const { name, quantity, units } = req.body;
         if (units !== "Gramos" && units !== "Miligramos" && units !== "Unidad" && units !== "Unidades") {
-            return res.status(400).json({ message: 'Unidad de medida mal introducida. Introduce: Gramos, Miligramos o Unidades' })
+            return res.status(400).json({ message: 'Unidad de medida mal introducida. Introduce: Gramos, Miligramos, Unidad o Unidades' })
         };
     
         const ingredientDuplicated = await Ingredient.findOne({ name, quantity });
@@ -29,7 +29,7 @@ const getAllIngredients = async (req, res, next) => {
     try {
         const allIngredients = await Ingredient.find();
         if (!allIngredients.length) {
-            return res.status(400).json({ message: "No tenemos ingredientes en nuestro stock." });
+            return res.status(400).json({ message: "Actualmente no tenemos ningún ingrediente." });
         }
         return res.status(200).json({ message: "Estos son los ingredientes que tenemos en stock:", allIngredients })
     } catch (error) {
@@ -44,7 +44,7 @@ const getIngredientByName = async (req, res, next) => {
         if (!findIngredientByName.length) {
             return res.status(400).json({ message: `No existe ningún ingrediente en nuestro stock que contenga '${name}'` });
         }
-        return res.status(200).json({ message: "Ingrediente encontrado:", findIngredientByName })
+        return res.status(200).json({ message: `Con la letra ${name} tenemos los siguientes ingredientes:`, findIngredientByName })
     } catch (error) {
         return res.status(400).json(`❌ Fallo en getIngredientByName: ${error.message}`)
     }
@@ -53,6 +53,12 @@ const getIngredientByName = async (req, res, next) => {
 const updateIngredient = async (req, res, next) => {
     try {
         const { id } = req.params;
+        const { quantity } = req.body;
+
+        if (quantity < 1) {
+            return res.status(400).json({ message: 'No puedes tener un ingrediente con cantidad 0.' })
+        }
+
         const ingredientModify = new Ingredient(req.body)
         ingredientModify._id = id;
         if (req.file) {
@@ -63,9 +69,9 @@ const updateIngredient = async (req, res, next) => {
 
         const ingredientUpdated = await Ingredient.findByIdAndUpdate(id, ingredientModify, { new: true });
         if (!ingredientUpdated){
-            return res.status(400).json({ message: 'No existe ese producto.' });
+            return res.status(400).json({ message: 'No existe ese ingrediente.' });
         }
-        return res.status(200).json({ message: 'Producto actualizado correctamente', ingredientUpdated });
+        return res.status(200).json({ message: 'Ingrediente actualizado correctamente', ingredientUpdated });
     } catch (error) {
         return res.status(400).json(`❌ Fallo en updateIngredient: ${error.message}`)
     }
@@ -79,7 +85,7 @@ const deleteIngredient = async (req, res, next) => {
             return res.status(400).json({ message: "Ese ingrediente ya no existe en nuestro stock." });
         }
         deleteFile(ingredientDeleted.img);
-        return res.status(200).json({ message: "Ingrediente eliminado de nuestro stock.", ingredientDeleted });
+        return res.status(200).json({ message: "Ingrediente eliminado correctamente de nuestro stock.", ingredientDeleted });
     } catch (error) {
         return res.status(400).json(`❌ Fallo en deleteIngredient: ${error.message}`)
     }
--- src/api/models/ingredient.js
+++ src/api/models/ingredient.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ingredientSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    quantity: { type: Number, required: true, trim: true },
+    quantity: { type: Number, required: true, min: 1, trim: true },
     units: { type: String, required: true, enum: ["Gramos", "Mililitros", "Unidad", "Unidades"] },
     img: { type: String, required: true, default: "Foto del ingrediente." },
 },{
