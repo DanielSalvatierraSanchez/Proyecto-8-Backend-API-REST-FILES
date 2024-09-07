c473d8a21fd27ea8e698a8f48ae2ea6b3952f6ba
dsalvatierra <dss250583@gmail.com>
2024-09-07 10:55:33 +0200

    FIX file.js uploadFolders test failure

--- src/api/routes/cake.js
+++ src/api/routes/cake.js
@@ -2,11 +2,9 @@ const cakeRoutes = require('express').Router();
 const upload = require('../../middleware/file');
 const { getCakeByName, getAllCakes, updateCake, deleteCake, createCake, deleteIngredientOfCake } = require('../controllers/cake');
 
-// cakeRoutes.post('/register', upload.single("firstImg"), createCake)
 cakeRoutes.post('/register', upload.fields([{ name: "firstImg" }, { name: "secondImg" }, { name: "thirdImg" }]), createCake)
 cakeRoutes.get('/getBy/:name', getCakeByName)
 cakeRoutes.get('/', getAllCakes)
-// cakeRoutes.put('/update/:id', upload.single("firstImg"), updateCake)
 cakeRoutes.put('/update/:id', upload.fields([{ name: "firstImg" }, { name: "secondImg" }, { name: "thirdImg" }]), updateCake)
 cakeRoutes.delete('/deleteIngredient/:id', deleteIngredientOfCake)
 cakeRoutes.delete('/delete/:id', deleteCake)
--- src/middleware/file.js
+++ src/middleware/file.js
@@ -12,4 +12,22 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage });
 
+//! Estuve probando el codigo de abajo pero no se me exportaba, si podeis me gustaria verlo detenidamente o donde puedo ver ejemplos
+//! ya que me parece muy interesante separar imagenes por carpetas, el orden es una de mis TOC mas preciados jajajajajaja
+// const foldersOfStorage = (folderName) => {
+//     return new CloudinaryStorage({
+//         cloudinary: cloudinary,
+//         params: {
+//             folder: folderName,
+//             allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp', 'avif'],
+//         }
+//     });
+// };
+
+// const uploadFolders = (folderName) => {
+//     const storage = foldersOfStorage(folderName);
+//     return multer({ storage });
+// };
+// module.exports = uploadFolders;
+
 module.exports = upload;
